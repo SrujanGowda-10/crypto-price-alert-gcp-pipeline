e5ce61fc5bca105227222d2eab29ea62cf19e1c5
SrujanGowda-10 <srujangowda1005@gmail.com>
2025-06-07 16:21:44 +0530

    Update crypto_alert_dag.py

--- crypto_alert_dag.py
+++ crypto_alert_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.providers.google.cloud.operators.functions import CloudFunctionInvokeFunctionOperator
-# from airflow.providers.google.cloud.operators.dataform import DataformRunWorkflowInvocationOperator
 from airflow.providers.google.cloud.operators.dataform import (
     DataformCreateCompilationResultOperator,
     DataformCreateWorkflowInvocationOperator
